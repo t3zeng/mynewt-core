4f4c56aa279e9601e4811f3c695262e0636431ac
Tian Zeng <tianyu@proxy.com>
2022-01-28 10:06:16 -0500

    Implement I2C read/write

--- hw/mcu/ambiq/apollo3/src/hal_i2c.c
+++ hw/mcu/ambiq/apollo3/src/hal_i2c.c
@@ -32,7 +32,8 @@
 #undef IOSLAVE
 #undef CLKGEN
 
-void *g_IOMHandle;
+/* Pointer array that points to am_hal_iom g_IOMhandles */
+am_hal_iom_state_t          *g_IOMhandles[AM_REG_IOM_NUM_MODULES];
 
 static am_hal_iom_config_t g_sIOMI2cConfig =
 {
@@ -116,14 +117,14 @@ hal_i2c_init(uint8_t i2c_num, void *usercfg)
     //
     // Initialize the IOM.
     //
-    am_hal_iom_initialize(i2c_num, &g_IOMHandle);
+    am_hal_iom_initialize(i2c_num, &g_IOMHandles[i2c_num]);
 
-    am_hal_iom_power_ctrl(g_IOMHandle, AM_HAL_SYSCTRL_WAKE, false);
+    am_hal_iom_power_ctrl(g_IOMHandles[i2c_num], AM_HAL_SYSCTRL_WAKE, false);
 
     //
     // Set the required configuration settings for the IOM.
     //
-    am_hal_iom_configure(g_IOMHandle, &g_sIOMI2cConfig);
+    am_hal_iom_configure(g_IOMHandles[i2c_num], &g_sIOMI2cConfig);
 
     /* Configure GPIOs for I2C based on i2c_num */
     pin_cfg = hal_i2c_pin_config(i2c_num, cfg);
@@ -139,6 +140,54 @@ hal_i2c_init(uint8_t i2c_num, void *usercfg)
 
     am_hal_gpio_pinconfig(cfg->sda_pin,  i2c_cfg);
     am_hal_gpio_pinconfig(cfg->scl_pin,  i2c_cfg);
+
+    return 0;
+}
+
+int
+hal_i2c_master_write(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
+                     uint32_t timeout, uint8_t last_op)
+{
+    am_hal_iom_transfer_t       Transaction;
+
+    Transaction.ui32InstrLen    = 0;
+    Transaction.ui32Instr       = 0;
+    Transaction.eDirection      = AM_HAL_IOM_TX;
+    Transaction.ui32NumBytes    = pdata->len;
+    Transaction.pui32TxBuffer   = (uint32_t *)pdata->buffer;
+    Transaction.bContinue       = !(bool)last_op;
+    Transaction.ui8RepeatCount  = 0;
+    Transaction.ui32PauseCondition = 0;
+    Transaction.ui32StatusSetClr = 0;
+    Transaction.uPeerInfo.ui32I2CDevAddr = pdata->address;
+
+    g_IOMHandles[i2c_num]->waitTimeout = timeout;
+    if (am_hal_iom_blocking_transfer(g_IOMHandles[i2c_num], &Transaction) != AM_HAL_STATUS_SUCCESS)
+        return -1;
+
+    return 0;
+}
+
+int
+hal_i2c_master_read(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
+                    uint32_t timeout, uint8_t last_op)
+{
+    am_hal_iom_transfer_t       Transaction;
+
+    Transaction.ui32InstrLen    = 0;
+    Transaction.ui32Instr       = 0;
+    Transaction.eDirection      = AM_HAL_IOM_RX;
+    Transaction.ui32NumBytes    = pdata->len;
+    Transaction.pui32RxBuffer   = (uint32_t *)pdata->buffer;
+    Transaction.bContinue       = !(bool)last_op;
+    Transaction.ui8RepeatCount  = 0;
+    Transaction.ui32PauseCondition = 0;
+    Transaction.ui32StatusSetClr = 0;
+    Transaction.uPeerInfo.ui32I2CDevAddr = pdata->address;
+
+    g_IOMHandles[i2c_num]->waitTimeout = timeout;
+    if (am_hal_iom_blocking_transfer(g_IOMHandles[i2c_num], &Transaction) != AM_HAL_STATUS_SUCCESS)
+        return -1;
 
     return 0;
 }
